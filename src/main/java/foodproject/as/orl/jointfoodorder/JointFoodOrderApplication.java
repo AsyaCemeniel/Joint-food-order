68d80d7c2ad11279fc9795a02eff747079b28274
AsyaCemeniel <asyaorlov89@gmail.com>
2021-01-10 15:26:08 +0200

    Initial commit

---
+++ src/main/java/foodproject/as/orl/jointfoodorder/JointFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package foodproject.as.orl.jointfoodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JointFoodOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JointFoodOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/foodproject/as/orl/jointfoodorder/JointFoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package foodproject.as.orl.jointfoodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JointFoodOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
